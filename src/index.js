bff002b4e9381dbee3141a4077fd59b2f05671c0
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-11 13:34:53 +0900

    TODOJS

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
   console.log(li);
+  const button1 = document.createElement("button");
+  button1.innerText = "完了";
+  button1.addEventListener("click", () => {
+    alert("完了");
+  });
+  const button2 = document.createElement("button");
+  button2.innerText = "削除";
+  button2.addEventListener("click", () => {
+    alert("削除");
+  });
+  console.log(button1, button2);
   div.appendChild(li);
+  div.appendChild(button1);
+  div.appendChild(button2);
   document.getElementById("incomplete-list").appendChild(div);
 };
 
